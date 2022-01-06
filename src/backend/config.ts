f1e79b85b6a8b2a400cdf3053ef0150ca8a5b6ad
Florian Pürschel <hello@prshl.de>
2022-01-06 21:27:08 +0100

    Delete config.ts

--- src/backend/config.ts
+++
@@ -1,8 +0,0 @@
-export const config = {
-	apiKey: 'AIzaSyCOF1pSxa39GBBQBt6J5Ni8fpvhALHB30Q',
-	authDomain: 'thesis-css.firebaseapp.com',
-	projectId: 'thesis-css',
-	storageBucket: 'thesis-css.appspot.com',
-	messagingSenderId: '365194454224',
-	appId: '1:365194454224:web:7137c907c7fd33f71dead5',
-};
